1cf1a66521a3f7798f35b6f341baf78da93436e7
Nikola Zjalic <nzjalic@gmail.com>
2024-10-28 23:36:27 -0400

    file rename

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import tkinter as tk
 from tkinter import filedialog
+import os
 
 class Watcher:
     def __init__(self, directory_to_watch):
@@ -20,23 +21,24 @@ class Watcher:
             self.observer.stop()
         self.observer.join()
 
-
 class Handler(FileSystemEventHandler):
     @staticmethod
     def on_created(event):
-        print(f"Created: {event.src_path}")
+        print(f"Created: {os.path.basename(event.src_path)}")
 
     @staticmethod
     def on_modified(event):
-        print(f"Modified: {event.src_path}")
+        print(f"Modified: {os.path.basename(event.src_path)}")
 
+    @staticmethod
+    def on_moved(event):
+        print(f"Renamed: {os.path.basename(event.src_path)} -> {os.path.basename(event.dest_path)}")
 
 def select_directory():
     root = tk.Tk()
     root.withdraw()  # Hide the main tkinter window
     folder_selected = filedialog.askdirectory()  # Open the folder selection dialog
     return folder_selected
-
 
 if __name__ == "__main__":
     print("Select a folder to watch...")
